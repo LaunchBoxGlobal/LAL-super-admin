5191934b941a90c97c0e4ba42d656fcc5d319b5d
shahrukhpunjwani <shahrukhali2520@gmail.com>
2026-03-26 17:11:09 +0500

    decreased navbar spacing

--- landing-page/src/components/Navbar.jsx
+++ landing-page/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-white/90 backdrop-blur-md shadow-sm py-4" : "bg-transparent py-4"}`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? "bg-white/90 backdrop-blur-md shadow-sm py-2" : "bg-transparent py-2"}`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <Link
