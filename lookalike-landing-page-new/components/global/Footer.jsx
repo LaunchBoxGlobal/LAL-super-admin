2d8ff471a50a2e8a7e7f5b9d99fbe7f38254f0e9
shoaib.muhammad <shoaib.muhammad@launchboxglobal.co>
2026-07-24 14:34:30 +0500

    added social links on landing page

--- lookalike-landing-page-new/components/global/Footer.jsx
+++ lookalike-landing-page-new/components/global/Footer.jsx
@@ -2,8 +2,35 @@
 
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSnapchat } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 
-// import { Link, useLocation, useNavigate } from "react-router-dom";
+const SOCIALS = [
+  {
+    icon: FaFacebook,
+    url: "https://www.facebook.com/lookalikematch/",
+  },
+  {
+    icon: AiFillInstagram,
+    url: "https://www.instagram.com/lookalike.match/",
+  },
+  {
+    icon: FaLinkedin,
+    url: "https://www.linkedin.com/company/lookalike-match/",
+  },
+  {
+    icon: FaSnapchat,
+    url: "https://lookalike.match.com",
+  },
+  {
+    icon: AiFillTikTok,
+    url: "https://lookalike.match.com",
+  },
+];
 
 const Footer = () => {
   //   const navigate = useNavigate();
@@ -44,9 +71,49 @@ const Footer = () => {
               similarity. Discover meaningful connections and real lookalikes
               every day.
             </p>
+
+            <ul className="flex items-center gap-3">
+              <li>
+                <Link
+                  href={`https://www.facebook.com/lookalikematch/`}
+                  target="_blank"
+                >
+                  <FaFacebook size={24} className="text-gray-400" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={`https://www.instagram.com/lookalike.match/`}
+                  target="_blank"
+                >
+                  <AiFillInstagram size={28} className="text-gray-400" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={`https://www.linkedin.com/company/lookalike-match/`}
+                  target="_blank"
+                >
+                  <FaLinkedin size={24} className="text-gray-400" />
+                </Link>
+              </li>
+              <li>
+                <Link href={`https://snapchat.com/t/gSq3ZVzq`} target="_blank">
+                  <FaSnapchat size={24} className="text-gray-400" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href={`https://www.tiktok.com/@lookalike.match?_r=1&_t=ZS-98ITPjyVD41`}
+                  target="_blank"
+                >
+                  <AiFillTikTok size={26} className="text-gray-400" />
+                </Link>
+              </li>
+            </ul>
           </div>
 
-          <div className="col-span-1 md:col-span-1 lg:pl-10">
+          <div className="col-span-1 md:col-span-1 lg:pl-20">
             <h4 className="text-white font-bold mb-4">Links</h4>
             <ul className="space-y-2 text-sm">
               <li>
